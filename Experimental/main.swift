b1a79fa57493aa8e8b0068052fa5cb260cb25303
Rob Bishop <GreatBigBore@gmail.com>
2018-11-29 18:23:45 -0800

    Maybe I finally have dispatch working, who knows

--- Experimental/main.swift
+++ Experimental/main.swift
@@ -18,7 +18,7 @@
 // IN THE SOFTWARE.
 //
   
-
+import Dispatch
 import Foundation
 
 #if EXPERIMENTAL
@@ -29,26 +29,197 @@ print("Experimental")
 print("Run dark")
 #endif
 
-protocol TSTestSubjectProtocol {
-    
+#if true// THIS_WORKS_WITH_MY_GLOBAL_AND_MY_SLEEP_NOT_HIS_MAIN
+let group = DispatchGroup()
+let queue = DispatchQueue(label: "com.theswiftdev.queues.serial")
+let workItem = DispatchWorkItem {
+    print("start")
+//    sleep(1)
+    print("end")
 }
 
-protocol SFSubjectFactoryProtocol {
-    
+queue.async(group: group) {
+    print("group start")
+//    sleep(2)
+    print("group end")
 }
 
-protocol FTFitnessTesterProtocol {
-    
+queue.async(group: group) { print("fuck this too!") }
+DispatchQueue.global().async(group: group, execute: workItem)
+
+// you can block your current queue and wait until the group is ready
+// a better way is to use a notification block instead of blocking
+//group.wait(timeout: .now() + .seconds(3))
+//print("done")
+
+group.notify(queue: queue) {
+    print("done")
+}
+sleep(1)
+
+#endif
+
+#if ANOTHER_THAT_WORKS_WITH_GLOBAL
+func load(delay: UInt32, completion: () -> Void) {
+    sleep(delay)
+    completion()
 }
 
-class TSTestSubject: TSTestSubjectProtocol {
-    
+let group = DispatchGroup()
+
+group.enter()
+load(delay: 1) {
+    print("1")
+    group.leave()
 }
 
-class SFSubjectFactory: SFSubjectFactoryProtocol {
-    
+group.enter()
+load(delay: 1) {
+    print("2")
+    group.leave()
 }
 
-class FTFitnessTester: FTFitnessTesterProtocol {
-    
+group.enter()
+load(delay: 1) {
+    print("3")
+    group.leave()
 }
+
+group.notify(queue: .global()) {
+    print("done")
+}
+#endif
+
+//let q = DispatchQueue.global()
+
+//q.async {
+//    print("this will return instantly")
+//}
+//
+//let text = q.sync {
+//    return "this will block"
+//}
+//print(text)
+
+
+//var workItem: DispatchWorkItem?
+//workItem = DispatchWorkItem {
+//    for i in 1..<6 {
+//        print("two\(i)")
+//        guard let item = workItem, !item.isCancelled else {
+//            print("cancelled")
+//            break
+//        }
+//        print(String(i))
+//    }
+//}
+//
+//workItem?.notify(queue: .main) {
+//    print("done")
+//}
+//
+//DispatchQueue.global().async {
+//    print("one")
+//    workItem?.cancel()
+//}
+//
+//print("fucking two")
+//DispatchQueue.main.async(execute: workItem!)
+//print("fucking three")
+//workItem!.wait()
+//print("fucking four")
+//sleep(2)
+//print("fucking five")
+//
+//// you can use perform to run on the current queue instead of queue.async(execute:)
+////workItem?.perform()
+//
+////
+//let background = DispatchQueue.global()
+//
+////func doSyncWork() {
+////    background.sync { for _ in 1...3 { print("Light") } }
+////
+////    for _ in 1...3 { print("Heavy") }
+////}
+////
+////doSyncWork()
+//
+//protocol Threadable {
+//    func go()
+//}
+//
+//class ThreadStuff: Threadable {
+//    let dg = DispatchGroup()
+//    var sp: DispatchWorkItem!
+//    var ex: DispatchWorkItem!
+//
+//    func spawn() { for _ in 0..<1000 {print("spawn")} }
+//    func execute() { for _ in 0..<1000 {print("execute")} }
+//
+//    func go() {
+//        print(".", terminator: "")
+//        sp = DispatchWorkItem(block: self.spawn)
+//        ex = DispatchWorkItem(block: self.execute)
+//        print("!", terminator: "")
+//        dg.enter()
+//        spawn()
+//        execute()
+//        dg.leave()
+//    }
+//}
+//
+#if MY_CODE_NO_FUCKING_WORKEE
+var defaultDQ = DispatchQueue.global()
+
+class FTFitnessTester {
+    let id: Int
+    let dg: DispatchGroup
+    var sp: DispatchWorkItem!
+
+    init(_ id: Int, dispatchGroup: DispatchGroup) {
+        self.id = id
+        self.dg = dispatchGroup
+    }
+
+    func go() {
+        sp = DispatchWorkItem(block: self.spawn)
+        sp.notify(queue: defaultDQ, execute: { print("Why am I not high?") })
+    }
+
+    func spawn() {
+        dg.enter(); print("FT \(id) spawning"); dg.leave()
+    }
+}
+
+func spawnTester() {
+
+}
+
+let dg = DispatchGroup()
+var testers = [FTFitnessTester]()
+
+for fishNumber in 0..<10 {
+    let ft = FTFitnessTester(fishNumber, dispatchGroup: dg)
+    testers.append(ft)
+    ft.go()
+}
+
+dg.notify(queue: defaultDQ, execute: { print("always!") })
+
+print("hell")
+//dg.activate()
+print("0")
+dg.wait()
+print("5h34")
+#endif
+//print("a", terminator: "")
+////let ts = ThreadStuff()
+////print("b", terminator: "")
+////ts.go()
+////print("c", terminator: "")
+//////print("gone")
+//////ts.dg.wait()
+//////ts.dg.notify(queue: background, execute: {print("how about this?") })
+////print("d", terminator: "")
+//////ts.dg.notify(queue: background, work: ts.ex)
