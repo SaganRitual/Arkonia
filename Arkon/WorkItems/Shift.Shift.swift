207b9800165c18710fa47216fa6a67d4c0238844
Rob Bishop <c.sagan.ritual@gmail.com>
2019-11-05 17:52:48 -0800

    Don't waste a move action on a stationary Arkon

--- Arkon/WorkItems/Shift.Shift.swift
+++ Arkon/WorkItems/Shift.Shift.swift
@@ -1,14 +1,19 @@
 import SpriteKit
 
 extension Shift {
+    static var ssCount = 0
     func shift(_ onComplete: @escaping () -> Void) {
         guard let st = self.shiftTarget else { fatalError() }
+
+        let stationary = (self.stepper.gridlet == st)
 
         oldGridlet = stepper.gridlet!
         self.stepper.gridlet = st
 
-        let moveAction =
-            SKAction.move(to: self.stepper.gridlet.scenePosition, duration: 0.1)
+        let moveDuration: TimeInterval = 0.1
+        let moveAction = stationary ?
+            SKAction.wait(forDuration: moveDuration) :
+            SKAction.move(to: self.stepper.gridlet.scenePosition, duration: moveDuration)
 
         stepper.sprite.run(moveAction) { onComplete() }
     }
