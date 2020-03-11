1f7f7bffceead88ccfc026413f0501126b6de0bf
Rob Bishop <c.sagan.ritual@gmail.com>
2020-03-10 18:16:42 -0700

    Defensive

--- Lifelets/Metabolize.swift
+++ Lifelets/Metabolize.swift
@@ -20,42 +20,30 @@ extension Metabolize {
 
 extension Metabolism {
     fileprivate func metabolizeProper(_ isStarving: Bool, _ nose: SKSpriteNode) {
-        if stomach.level > 0 {
-            Debug.log(level: 96) { "metabolizeProper; stomach = \(stomach.level) (\(stomach.level / stomach.capacity)) oxygen = \(oxygenLevel)" }
-        }
-
         nose.color = .green
         nose.colorBlendFactor = min(fungibleEnergyFullness * 2, 1)
 
-        var logMessage = "mp:"
-
         let stomachToReady = !stomach.isEmpty && !readyEnergyReserves.isFull
-//        logMessage += " stomachToReady = \(stomachToReady), ready is full = \(readyEnergyReserves.isFull)"
 
         if stomachToReady {
             let transfer = stomach.withdraw(Arkonia.energyTransferRateInJoules)
             readyEnergyReserves.deposit(transfer)
-            logMessage += ", transfer \(transfer) stomach->ready"
         }
 
         let readyToFat = readyEnergyReserves.isAmple && !fatReserves.isFull
-//        logMessage += ", readyToFat = \(readyToFat)"
 
         if readyToFat {
             let surplus_ = readyEnergyReserves.level - readyEnergyReserves.overflowThreshold
             let surplus = min(surplus_, Arkonia.energyTransferRateInJoules)
             let net = readyEnergyReserves.withdraw(surplus)
             fatReserves.deposit(net)
-            logMessage += ", \(net) ready->fat"
         }
 
         let tapFatReserves = (readyEnergyReserves.level < reUnderflowThreshold)
-//        logMessage += ", tapFatReserves = \(tapFatReserves)"
 
         if tapFatReserves {
             let refill = fatReserves.withdraw(Arkonia.energyTransferRateInJoules)
             readyEnergyReserves.deposit(refill)
-            logMessage += ", \(refill) fat->ready"
         }
 
         let fatToSpawn = fatReserves.isAmple && !spawnReserves.isFull
@@ -63,11 +51,6 @@ extension Metabolism {
         if fatToSpawn {
             let transfer = fatReserves.withdraw(Arkonia.energyTransferRateInJoules)
             spawnReserves.deposit(transfer)
-            logMessage += ", \(transfer) fat->spawn"
-        }
-
-        if logMessage.count > 3 {
-            Debug.log(level: 96) { logMessage }
         }
     }
 }
