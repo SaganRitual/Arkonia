4662b05bcec109dd1771867ffa5903f68aa8970e
Rob Bishop <c.sagan.ritual@gmail.com>
2020-06-12 11:22:59 -0700

    Tweaks to energy budget to increase survival rates

--- Metabolism/EnergyBudget.swift
+++ Metabolism/EnergyBudget.swift
@@ -49,7 +49,7 @@ struct EnergyBudget: HasCapacity {
 }
 
 extension EnergyBudget {
-    static let supersizer: CGFloat = 20
+    static let supersizer: CGFloat = 5
 
     static func makeEnergyBudgetForChamberedStore(_ organID: OrganID, _ chamberID: ChamberID) -> EnergyBudget {
         switch chamberID {
